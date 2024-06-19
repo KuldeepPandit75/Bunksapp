e951bec0a366b4742f715600c49d1c920e66513b
KuldeepPandit75 <kuldeepk11139@gmail.com>
2024-06-19 18:37:56 +0530

    added chat request feature

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ main()
     .catch(err => console.log(err));
 
 async function main() {
-    await mongoose.connect("mongodb+srv://kuldeeppandat:kpvaultserver8055@cluster1.rbtvmid.mongodb.net/whatsapp?retryWrites=true&w=majority&appName=Cluster1");
+    await mongoose.connect("mongodb://localhost:27017/whatsapp");
 }
 
 app.get("/", (req, res) => {
